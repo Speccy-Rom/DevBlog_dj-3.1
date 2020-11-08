cf3352f45e490b665c9b3a4d66bfe2eccd6438d2
speccy-rom <speccy.Rom@gmail.comspeccy.Rom@gmail.comspeccy.rom@gmail.com>
2020-11-08 19:16:54 +0300

    Работа с модул. и адм.+ test BooleanField(null=True)

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import News
+
+
+def index(request):
+    news = News.objects.all()
+    contect = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, 'blog/index.html', contect)
+
+
+
