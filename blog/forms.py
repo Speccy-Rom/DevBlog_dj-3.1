92ba187598708bb0b4f46626c4ee1c46d582b1b6
Speccy-rom <speccy.rom@gmail.com>
2020-12-04 23:04:40 +0300

    добавил кастомный валидатор(title)

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,9 @@
 from django import forms
-#from .models import Category
+# from .models import Category
 from .models import News
+import re
+from django.core.exceptions import ValidationError
+
 
 ## Форма не связанная с моделями (БД)
 # class NewsForm(forms.Form):
@@ -23,6 +26,10 @@ class NewsForm(forms.ModelForm):
             'title': forms.TextInput(attrs={"class": "form-control"}),
             'content': forms.Textarea(attrs={"class": "form-control", "rows": 5}),
             'category': forms.Select(attrs={"class": "form-control"})
-
-
         }
+
+    def clean_title(self):  # кастомный валидатор
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
