82630e522485e4639832b87b71fe502e1157cff5
Speccy-rom <speccy.rom@gmail.com>
2020-12-13 18:55:14 +0300

    заменил def Category на класс LastView в category

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@ class HomeNews(ListView):
     model = News
     template_name = 'blog/home.html'
     context_object_name = 'blog'
+
     # extra_context = {'title': 'Главная'}
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -18,6 +19,23 @@ class HomeNews(ListView):
 
     def get_queryset(self):
         return News.objects.filter(is_published=True)  # снимает с будликации запись (со стороны фронтенда)
+
+
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'blog/home.html'
+    context_object_name = 'blog'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'],
+                                   is_published=True)  # снимает с будликации запись (со стороны фронтенда)
+
 
 
 
@@ -30,15 +48,15 @@ class HomeNews(ListView):
 #     return render(request, 'blog/index.html', context)
 
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    # category = Category.objects.get(pk=category_id)
-    category = get_object_or_404(Category, pk=category_id)
-    context_category = {
-        'news': news,
-        'category': category,
-    }
-    return render(request, 'blog/category.html', context_category)
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     # category = Category.objects.get(pk=category_id)
+#     category = get_object_or_404(Category, pk=category_id)
+#     context_category = {
+#         'news': news,
+#         'category': category,
+#     }
+#     return render(request, 'blog/category.html', context_category)
 
 
 def view_news(request, news_id):
@@ -49,12 +67,12 @@ def view_news(request, news_id):
 
 def add_news(request):
     if request.method == 'POST':
-        form = NewsForm(request.POST) # форма связана с данными
-        if form.is_valid(): # условие прошла ли форма валидацию
+        form = NewsForm(request.POST)  # форма связана с данными
+        if form.is_valid():  # условие прошла ли форма валидацию
             # print(form.cleaned_data)
             # news = News.objects.create(**form.cleaned_data) # сохраняем данные введеные через форму не связанной с моделями
-            news = form.save() # применяем метод save для форм связанных с моделями БД
-            return redirect(news) # редирект на страницу новости
+            news = form.save()  # применяем метод save для форм связанных с моделями БД
+            return redirect(news)  # редирект на страницу новости
     else:
-        form = NewsForm() # форма не связана с данными
+        form = NewsForm()  # форма не связана с данными
     return render(request, 'blog/add_news.html', {'form': form})
